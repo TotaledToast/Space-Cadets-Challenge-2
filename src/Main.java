b9338aa5b2e16cfb63144a7d928457b1867d1550
TotaledToast <totaledtoast@gmail.com>
2023-10-08 13:19:29 +0100

    Making changes, still very basic

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,38 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        //clear X;
-        //incr X;
-        //incr X;
-        //incr X;
-        //while X not 0 do;
-        //   decr X;
-        //end;
-        String[] Things = {"clear X;","incr X;","while X not 0 do;","   decr X;","end;"};
 
-        for (String element : Things){
-            System.out.println(element);
-            //System.out.println(element.matches("(\\s*)decr (.+);"));
-            //System.out.println(element.replaceAll("(\\s*)decr (.+);", "$2"));
+    List<String> programInput = new ArrayList<String>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the program file path: ");
+        String path = reader.nextLine();
+        reader.close();
+        Program_Runner program = new Program_Runner(path);
+
+
+    }
+
+
+
+}
+
+class Program_Runner {
+    List<String> programInput = new ArrayList<String>();
+
+    public Program_Runner(String fileName) throws FileNotFoundException {
+        fileReader(fileName);
+    }
+
+    void fileReader(String fileName) throws FileNotFoundException {
+        File input = new File(fileName);
+        Scanner fileReader = new Scanner(input);
+        while (fileReader.hasNextLine()){
+            String line = fileReader.nextLine();
+            programInput.add(line);
         }
+        fileReader.close();
     }
 }
